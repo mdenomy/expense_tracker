72be84ee3b9f2f8385dfd1caab606e44b6314b32
Michael Denomy <mdenomy@gmail.com>
2013-01-12 10:50:15 -0500

    Failing view index tests

---
+++ spec/views/index.html.erb_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe "expenses/index" do
+
+  it "shows every expense" do
+    assign(:expenses, [stub_model(Expense), stub_model(Expense), stub_model(Expense)])
+    render
+    assert_select 'tr.expense', 3
+  end
+
+  it "has a header row" do
+    assign(:expenses, [stub_model(Expense), stub_model(Expense), stub_model(Expense)])
+    render
+    assert_select 'tr.expense_header'
+    assert_select 'th', text: "Amount"
+    assert_select 'th', text: "Description"
+  end
+
+  it "expense has the amount and description" do
+    assign(:expenses, [ stub_model(Expense, amount: 1.25, description: "Snickers bar")])
+    render
+    expect(rendered).to include("1.25")
+    expect(rendered).to include("Snickers bar")
+  end
+
+end
+
