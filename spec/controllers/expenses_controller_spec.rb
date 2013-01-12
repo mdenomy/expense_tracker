1e12fa654184560e171d51f8ea6ee2da9f128fce
Michael Denomy <mdenomy@gmail.com>
2013-01-12 14:25:17 -0500

    Failing test for new expense action

--- spec/controllers/expenses_controller_spec.rb
+++ spec/controllers/expenses_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe ExpensesController do
 
   describe "GET 'index'" do
-    it "returns http success" do
+    it "returns success" do
       get 'index'
       response.should be_success
     end
@@ -11,6 +11,14 @@ describe ExpensesController do
     it "assigns expenses" do
       get 'index'
       assigns(:expenses).should_not be_nil
+    end
+  end
+
+  describe "GET 'new'" do
+
+    it "returns success" do
+      get 'new'
+      response.should be_success
     end
   end
 end
