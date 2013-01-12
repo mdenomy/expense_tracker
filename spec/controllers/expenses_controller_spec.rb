2c6c85b0fa30a1d9b2e80add9edec0a0ad82cb08
Michael Denomy <mdenomy@gmail.com>
2013-01-11 20:54:18 -0500

    Failing controller get index route

---
+++ spec/controllers/expenses_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe ExpensesController do
+
+  describe "GET 'index'" do
+    it "returns http success" do
+      get 'index'
+      response.should be_success
+    end
+  end
+end
