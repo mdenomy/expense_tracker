d3a4d17bc957ec2ae0eaef04c1b84bafb3215f57
Michael Denomy <mdenomy@gmail.com>
2013-01-11 19:07:19 -0500

    Refactored tests to move expense creation to method

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -3,24 +3,25 @@ require 'spec_helper'
 describe Expense do
 
   describe "validation" do
+
+    def expense_with_amount(amount)
+      Expense.new(amount:amount, description: "Some expense")
+    end
+
     it "negative amounts are not valid" do
-        expense = Expense.new(amount: -1, description: "Some expense")
-        expense.should_not be_valid
+        expense_with_amount(-1).should_not be_valid
     end
 
     it "amount of zero is not valid" do
-      expense = Expense.new(amount: 0, description: "Some expense")
-      expense.should_not be_valid
+      expense_with_amount(0).should_not be_valid
     end
 
     it "positive amounts are valid" do
-      expense = Expense.new(amount: 1, description: "Some expense")
-      expense.should be_valid
-    end
+      expense_with_amount(1).should be_valid
+     end
 
     it "needs a description" do
-      expense = Expense.new(amount: 1)
-      expense.should_not be_valid
+      Expense.new(amount: 1).should_not be_valid
     end
   end
 
