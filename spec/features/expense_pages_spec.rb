2d91da58e14b74d19f191ec778ae7ff2877dae11
Michael Denomy <mdenomy@gmail.com>
2013-01-13 11:36:52 -0500

    Refactored tests to reduce duplication

--- spec/features/expense_pages_spec.rb
+++ spec/features/expense_pages_spec.rb
@@ -2,19 +2,22 @@ require 'spec_helper'
 
 describe "Expense Pages" do
 
+  def is_on_expenses_page?(page)
+    page.should have_selector('h1', text: 'Expense List')
+    page.should have_selector('.expense_header')
+  end
+
   describe "root path" do
     it "maps to expenses path" do
       visit root_path
-      page.should have_selector('h1', text: 'Expense List')
-      page.should have_selector('.expense_header')
+      is_on_expenses_page?(page)
     end
   end
 
   describe "expenses index" do
     it "has the expense list" do
       visit expenses_path
-      page.should have_selector('h1', text: 'Expense List')
-      page.should have_selector('.expense_header')
+      is_on_expenses_page?(page)
     end
   end
 
@@ -37,24 +40,25 @@ describe "Expense Pages" do
 
       it "new expense is in the expenses list" do
         click_button "Add Expense"
+        is_on_expenses_page?(page)
         page.should have_content('54.99')
         page.should have_content('Bulleit Bourbon')
       end
     end
 
     describe "creating expense with invalid data" do
-      it "does not create a new expense" do
+      before do
         visit new_expense_path
         fill_in "Amount", with: -100.0
+      end
+
+      it "does not create a new expense" do
         expect { click_button "Add Expense" }.not_to change(Expense, :count)
       end
 
       it "stays on the new page" do
-        visit new_expense_path
-        fill_in "Amount", with: -100.0
         click_button "Add Expense"
-        fill_in "Amount", with: 110.0
-        fill_in "Description", with: "T Pass"
+        page.should have_selector('h1', text: 'Add New Expense')
       end
     end
   end
