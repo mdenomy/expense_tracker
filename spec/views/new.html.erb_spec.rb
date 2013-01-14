a5ef9f1126a7b1e460b23ae449a64623ca2a0b87
Michael Denomy <mdenomy@gmail.com>
2013-01-14 17:45:05 -0500

    Changed input of currency to string

--- spec/views/new.html.erb_spec.rb
+++ spec/views/new.html.erb_spec.rb
@@ -6,8 +6,8 @@ describe "expenses/new" do
     assign(:expense, stub_model(Expense).as_new_record)
     render
     assert_select "form", :action => expenses_path, :method => "post" do
-      assert_select "input#expense_amount", :name => "expense[amount]"
-      assert_select "input#expense_description", :name => "expense[description]"
+      assert_select "input#expense_amount[type=text]", :name => "expense[amount]"
+      assert_select "input#expense_description[type=text]", :type => "text", :name => "expense[description]"
       assert_select "input[type=submit]"
     end
   end
